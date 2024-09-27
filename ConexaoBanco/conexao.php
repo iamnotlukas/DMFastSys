558923f6ea3b4c1fba05a1fe865489f2f3ec3007
Dias <lucas.andrade.dias21@gmail.com>
2024-09-26 22:36:14 -0300

    add script de conexao com bd

---
+++ ConexaoBanco/conexao.php
@@ -0,0 +1,21 @@
+<?php
+// Configurações do banco de dados
+$host = 'localhost';    // Host do banco de dados (geralmente localhost)
+$dbname = 'dmfastsys';   // Nome do banco de dados
+$user = 'root';          // Nome de usuário do MySQL
+$pass = '';              // Senha do MySQL
+
+try {
+    // Criando uma nova instância de PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+
+    // Configura o modo de erro para lançar exceções
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Conexão com o banco de dados bem-sucedida!";
+} catch (PDOException $e) {
+    // Caso ocorra algum erro na conexão, ele será capturado aqui
+    echo "Erro na conexão com o banco de dados: " . $e->getMessage();
+    exit;
+}
+?>
